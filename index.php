d72357ec8189ea0d4ff6aa679f66598b1ff5a82f
ShivanshuKaushal <shivanshu02@algomau.ca>
2024-05-17 15:14:37 +0000

    Created a login form

--- index.php
+++ index.php
@@ -4,5 +4,5 @@
   </head>
   <body>
     <h1>Assignment1</h1> 
-
+    <p><a href="/login.php">Click here to login </a></p>
 </html>
---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+   <title>Shivanshu</title>
+  </head>
+  <body>
+  <h1>Login Page</h1>
+    <form action="/validate.php" method="post">
+      <label for="username">Username:</label>
+      <br>
+      <input type="text" id="Username" name="Username" >
+      <br>
+      <label for="password">Password:</label>
+      <br>
+      <input type="password" id="Password" name="password">
+      <br><br>
+      <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
