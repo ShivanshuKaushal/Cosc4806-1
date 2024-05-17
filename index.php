a5ef71d9de03235eae8ee165ee6b198d332e7d41
ShivanshuKaushal <shivanshu02@algomau.ca>
2024-05-17 15:33:00 +0000

    added Session, Count of failed attempts and redirect to index

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
+<?php
+session_start();
+?>
+
 <html>
   <head>
     <title>Shivanshu</title>
   </head>
   <body>
     <h1>Assignment1</h1> 
+    
+    <p> Welcome, <?=$_SESSION['username'] ?></p>
+    
     <p><a href="/login.php">Click here to login </a></p>
 </html>
--- validate.php
+++ validate.php
@@ -1,14 +1,21 @@
 <?php
+session_start();
 
 $valid_username = "mike";
 $valid_password = "password";
 
 $username = $_REQUEST['username'];
+$SESSION['username'] = $username;
 $password = $_REQUEST['password'];
 
 if ($valid_username == $username && $valid_password == $password){
-  echo "success";
+    header ('location: /');
 } else {
-  echo "fail";
+  if (!isset($_SESSION['failed_attempts'])){
+    $_SESSION['failed_attempts'] = 1;
+  } else {
+    $_SESSION['failed_attempts'] = $_SESSION['failed_attempts'] + 1;
+  }
+  echo " This is unsuccessful attempt number: " . $_SESSION['failed_attempts'];
 }
 ?>
